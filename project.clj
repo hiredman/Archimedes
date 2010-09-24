6b48f25a02748e1dbdf607c8e6a1f8f77b248053
Kevin Downey (hiredman) <redchin@gmail.com>
2010-09-24 00:32:33 -0700

    bring in contrib for logging

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject Archimedes "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :namespaces [Archimedes.core])
