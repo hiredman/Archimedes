b147a78af9d82edd8e4686ee41bfa80924341b00
Kevin Downey <redchin@gmail.com>
2013-09-19 23:57:36 -0500

    working lein build

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
-(defproject Archimedes "1.0.0-SNAPSHOT"
+(defproject archimedes "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 ;; [org.clojure.contrib/monads "1.3.0-SNAPSHOT"]
-                 ;; [org.clojure.contrib/logging "1.3.0-SNAPSHOT"]
-                 ;; [log4j "1.2.16"]
                  [org.clojure/core.logic "0.7.5"]]
-  ;;  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
-  ;;  :namespaces [Archimedes.core]
   :java-source-paths ["java"]
-  :aliases {"build" ["do" "run" "-m" "Archimedes.bar/code-gen" "java/archimedes/MathOps.java," "javac"]}
-  )
+  ;; TODO: lein issue
+  :aliases {"build" ["do"
+                     "update-in" ":prep-tasks" "empty" "--"
+                     "run" "-m" "Archimedes.bar/code-gen" "java/archimedes/MathOps.java" "MathOps,"
+                     "javac"]})
